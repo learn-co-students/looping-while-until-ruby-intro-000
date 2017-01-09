f69a2255418119241d448697346885fc6d624a75
Sam Landfried <samlandfried@gmail.com>
2017-01-09 03:47:05 +0000

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,13 @@
 def using_while
 	levitation_force = 6
-	
+	spell = ""
+
+	while levitation_force < 10
+		spell += "Wingardium Leviosa\n"
+		levitation_force += 1
+	end
+
+	print spell
+
 	#your code here
 end
-
-
