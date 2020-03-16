051fb57dad08e73de7aade52c16ead83cdabb5f4
Jennifer Hernandez <jennifer19hernandez96@gmail.com>
2020-03-16 12:24:31 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,16 +1,10 @@
-
+levitation_force = 6
 def using_until
- levitation_force = 10
- loop do
-   puts "Wingardium Leviosa"
-end
-   levitation_force = 10
-    until levitation_force == 10 do
+  loop do
+    puts "Wingardium Leviosa"
+    until levitation_force == 10
       puts "Wingardium Leviosa"
       levitation_force += 1
-      break if levitation force == 10
     end
     end
   end
-
-using_until
--- lib/while.rb
+++ lib/while.rb
@@ -1,14 +1,10 @@
+levitation_force = 6
 def using_while
-	levitation_force = 10
-	loop do
-		puts "Wingardium Leviods"
-	end
-  while levitation_force < 10 do
+  loop do
     puts "Wingardium Leviosa"
-    levitation_force += 1
-		break if levitation force < 10
-    end
+  while levitation_force < 10
+        levitation_force += 1
+    puts "Wingardium Leviosa"
 end
 end
-
-using_while
+end
