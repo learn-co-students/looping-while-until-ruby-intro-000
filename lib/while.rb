deced8c4b013243cf1c90e36e0094867bb2082d9
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-04-10 14:54:39 +0000

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,16 +1,9 @@
-
 def using_while
-  levitation_force = 6
+levitation_force = 6
 
-loop do
-levitation_force = levitation_force + 1
-	puts "Wingardium Leviosa #{levitation_force}""
-	if levitation_force < 10
-end
-
-	until levitation_force == 10
-  puts "Wingardium Leviosa"
-  levitation_force >= 10
-  break
-  end
+while levitation_force < 10
+puts "Wingardium Leviosa"
+levitation_force += 1
+break if levitation_force >= 10
+   end
 end
