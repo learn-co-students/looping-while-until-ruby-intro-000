7f0b10a54fa91682bbf6c5a4b6d87ee5f2df342c
pleasant-communicator-3558 <sujchand2@gmail.com>
2020-10-27 22:59:54 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,8 @@
 def using_until
   levitation_force = 6
   until levitation = = 10 
-    puts "Wingardium Leviosa"
     levitation_force += 1 
+    puts "Wingardium Leviosa"
   end
     
 end
--- lib/while.rb
+++ lib/while.rb
@@ -1,8 +1,8 @@
 def using_while
   levitation_force = 6
   while levitation_force < 10 
-    puts "Wingardium Leviosa"
     levitation += 1 
+    puts "Wingardium Leviosa"
   end
     
 end
